9690b571d337b7c7c28e3fb70fc9826d367fe285
Joao Bernardo <jb.amaro95@gmail.com>
2021-01-09 14:43:26 +0000

    Updates mix.exs to use fetch the taskid dependency from Hex

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule TaskidDemoApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:taskid, path: "../taskid-elixir"}
+      {:taskid, "~> 0.1.0"}
     ]
   end
 
